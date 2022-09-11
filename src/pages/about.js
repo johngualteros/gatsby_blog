be4d50faf4139386cecd439e65be3f37bd2c1675
John Gualteros <gualterosjohn40@gmail.com>
2022-09-11 18:26:12 -0500

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const about = () => {
+    return (
+        <h1>About me</h1>
+    );
+}
+export default about;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Helmet from "react-helmet"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -24,6 +25,10 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
+      <Seo title="All posts" />
+      <Helmet>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Helmet>
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
